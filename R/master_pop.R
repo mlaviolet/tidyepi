7c4236928f25639e65958e1ccc39e220f0076474
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2019-05-23 13:45:32 -0400

    Added docs for table master_tbl and list dist_lst

---
+++ R/master_pop.R
@@ -0,0 +1,19 @@
+#' STandard population master table.
+#'
+#' Standard 2000 master population for direct age adjustment, from National Center for Health Statistics
+#' 
+#' @docType data
+#'
+#' @usage data(master_tbl)
+#' 
+#' @format A data table with 24 rows and 2 variables.
+#' \describe{
+#'   \item{\code{age_group}}{Age group}
+#'   \item{\code{pop}}{Standard population, in thousands}
+#'  }
+#' 
+#' #' @references Klein RJ, Schoenborn CA. Age adjustment using the 2000
+#' projected U.S. population. Healthy People Statistical Notes, no. 20.
+#' Hyattsville, Maryland: National Center for Health Statistics. January 2001.
+#' https://www.cdc.gov/nchs/data/statnt/statnt20.pdf
+"master_tbl"
---
+++ R/std_pop.R
@@ -0,0 +1,15 @@
+#' Standard population distributions.
+#'
+#' Standard 2000 populations for direct age adjustment, from National Center for Health Statistics
+#' 
+#' @docType data
+#'
+#' @usage data(dist_lst)
+#' 
+#' @format A named list with 24 named vectors: master age grouping, SEER age grouping, and distributions #1 through #22 as published by NCHS
+#' 
+#' @references Klein RJ, Schoenborn CA. Age adjustment using the 2000
+#' projected U.S. population. Healthy People Statistical Notes, no. 20.
+#' Hyattsville, Maryland: National Center for Health Statistics. January 2001.
+#' https://www.cdc.gov/nchs/data/statnt/statnt20.pdf
+"dist_lst" 
