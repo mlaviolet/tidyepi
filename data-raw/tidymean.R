92c43e14ec47f5c8a43c566440d7e634aafa594f
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2019-07-11 10:12:18 -0400

    Change data_var from string to bare name

--- data-raw/tidymean.R
+++ data-raw/tidymean.R
@@ -2,13 +2,14 @@
 #'
 #' @param df A data frame.
 #' @param group_var Grouping variable, as bare name.
-#' @param data_var Analysis variable, as a string.
+#' @param data_var Analysis variable, as bare name.
 #' @return Table of group means.
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarize
 #' @export
 # this version uses bare name for data_var
 tidy_mean <- function(df, group_var, data_var) {
+  # make data_var into string for use in name of summary column
   mean_name <- paste0("mean_", quo_name(enquo(data_var)))
   df %>%
     group_by({{ group_var }}) %>%
