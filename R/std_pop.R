9e518c77087d027616ac36b69817370e7b4fb9cb
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-02-05 11:33:50 -0500

    Add standard population distribution by single year of age

--- R/std_pop.R
+++ R/std_pop.R
@@ -15,3 +15,27 @@
 #' Hyattsville, Maryland: National Center for Health Statistics. January 2001.
 #' https://www.cdc.gov/nchs/data/statnt/statnt20.pdf
 "std_pop_list" 
+
+#' Standard population distributions by single year of age.
+#'
+#' Standard 2000 populations for direct age adjustment, from National Center for Health Statistics
+#' 
+#' @docType data
+#'
+#' @usage data(std_pop_single_age)
+#' 
+#' @format A data frame with two columns:
+#'  \describe{
+#'   \item{\code{age}}{Year of age from 0 to 100+}
+#'   \item{\code{population}}{Standard population in thousands.}
+#'   }
+#' 
+#' @references US Bureau of the Census. Population projections of the United States by age, sex, race, and Hispanic origin: 1995 to 2050 (P25-1130). February 1996. 
+#' 
+#' @references National Cancer Institute, Surveillance, Epidemiology, and End Results Progam. Standard populations (millions) for age-adjustment.
+#' https://seer.cancer.gov/stdpopulations
+#' 
+#' @keywords datasets
+#' 
+#' @source {US Bureau of the Census} \url{https://www.census.gov/prod/1/pop/p25-1130/p251130.pdf}
+"std_pop_single_age"
