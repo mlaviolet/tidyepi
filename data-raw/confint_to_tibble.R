8a125b429a5b966eaa94a06c926d5cb87d97bf0b
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-10-23 15:30:06 -0400

    Add multiplier argument and clean up

--- data-raw/confint_to_tibble.R
+++ data-raw/confint_to_tibble.R
@@ -6,7 +6,7 @@ library(broom)
 library(exactci)
 library(here)
 
-f2 <- function(df, x, n) {
+f2 <- function(df, x, n, multiplier = 100000) {
   df %>% 
     # rename data variables to match arguments
     rename_with(~ "x", {{ x }}) %>% 
@@ -15,35 +15,20 @@ f2 <- function(df, x, n) {
     nest() %>%
     mutate(ptest = map(data, ~ tidy(poisson.exact(x, n)))) %>%
     unnest(ptest) %>% 
-    select(x, n, estimate, conf.low, conf.high)
+    select(Count = x, Population = n, rate = estimate, rate_low = conf.low, 
+           rate_high = conf.high) %>% 
+    mutate(across(starts_with("rate"), ~ multiplier * .x))
   }
 
 df <- data.frame(id = c("A", "B", "C"),  x = c(0, 10, 10),
                  n = c(100, 400, 400))
 df %>% 
   group_by(id) %>% 
-  group_modify(~ f2(.x, x, n))
-
-# GROUPING BY RECORD ID WOULD OCCUR OUTSIDE WORKING FUNCTION
-# nest() and unnest() would probably not be needed
-# key is using tidy() to put test results in data frame
-# df %>%
-#   group_by(id, x, n) %>%
-#   nest() %>%
-#   mutate(ptest = map(data, ~ tidy(poisson.exact(x, n)))) %>%
-#   unnest(ptest) %>% 
-#   select(x, n, estimate, conf.low, conf.high)
+  group_modify(~ f2(.x, x, n, 1000))
 
 # with real data
 df2 <- read_tsv(here("data-raw", "Poisson rates.txt"), n_max = 6) %>% 
   select(State = States, Count, Population, Crude_Rate = `Crude Rate`) 
-
-# df2 %>%
-#   group_by(State, Count, Population) %>%
-#   nest() %>%
-#   mutate(ptest = map(data, ~ tidy(poisson.exact(Count, Population)))) %>%
-#   unnest(ptest) %>% 
-#   select(Count, Population, estimate, conf.low, conf.high)
 
 df2 %>% 
   filter(State == "New Hampshire") %>% 
@@ -53,7 +38,6 @@ df2 %>%
   group_by(State) %>% 
   group_modify(~ f2(.x, Count, Population))
 
-# fix names to match
 
 
 
