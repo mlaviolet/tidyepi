83999dd92b77e853b50418fb1e27a5a42e0c2b37
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-10-23 14:55:13 -0400

    Rename variables to match arguments

--- data-raw/confint_to_tibble.R
+++ data-raw/confint_to_tibble.R
@@ -6,8 +6,8 @@ library(broom)
 library(exactci)
 library(here)
 
-df <- data.frame(id = c("A", "B", "C"),  x = c(0, 10, 10), 
-                 n = c(100, 400, 400))
+df <- data.frame(id = c("A", "B", "C"),  Count = c(0, 10, 10), 
+                 Population = c(100, 400, 400))
 # GROUPING BY RECORD ID WOULD OCCUR OUTSIDE WORKING FUNCTION
 # nest() and unnest() would probably not be needed
 # key is using tidy() to put test results in data frame
@@ -30,13 +30,16 @@ df2 %>%
   select(Count, Population, estimate, conf.low, conf.high)
    
 
-f2 <- function(df, Count, Population) {
+f2 <- function(df, x, n) {
   df %>% 
-    group_by(Count, Population) %>%
+    # rename data variables to match arguments
+  rename_with(~ "x", {{ x }}) %>% 
+    rename_with(~ "n", {{ n }}) %>% 
+    group_by(x, n) %>%
     nest() %>%
-    mutate(ptest = map(data, ~ tidy(poisson.exact(Count, Population)))) %>%
+    mutate(ptest = map(data, ~ tidy(poisson.exact(x, n)))) %>%
     unnest(ptest) %>% 
-    select(Count, Population, estimate, conf.low, conf.high)
+    select(x, n, estimate, conf.low, conf.high)
   }
 
 df2 %>% 
@@ -47,6 +50,9 @@ df2 %>%
   group_by(State) %>% 
   group_modify(~ f2(.x, Count, Population))
 
-
+# fix names to match
+df %>% 
+  group_by(id) %>% 
+  group_modify(~ f2(.x, Count, Population))
 
 
