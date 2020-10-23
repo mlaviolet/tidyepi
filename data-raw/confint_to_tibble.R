17fe08dafd8062a12031216387c04ffd9f3016e9
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-10-23 15:13:39 -0400

    Confirmed working when data argument names don't match function argument names

--- data-raw/confint_to_tibble.R
+++ data-raw/confint_to_tibble.R
@@ -6,34 +6,10 @@ library(broom)
 library(exactci)
 library(here)
 
-df <- data.frame(id = c("A", "B", "C"),  Count = c(0, 10, 10), 
-                 Population = c(100, 400, 400))
-# GROUPING BY RECORD ID WOULD OCCUR OUTSIDE WORKING FUNCTION
-# nest() and unnest() would probably not be needed
-# key is using tidy() to put test results in data frame
-df %>%
-  group_by(id, x, n) %>%
-  nest() %>%
-  mutate(ptest = map(data, ~ tidy(poisson.exact(x, n)))) %>%
-  unnest(ptest) %>% 
-  select(x, n, estimate, conf.low, conf.high)
-
-# with real data
-df2 <- read_tsv(here("data-raw", "Poisson rates.txt"), n_max = 6) %>% 
-  select(State = States, Count, Population, Crude_Rate = `Crude Rate`) 
-
-df2 %>%
-  group_by(State, Count, Population) %>%
-  nest() %>%
-  mutate(ptest = map(data, ~ tidy(poisson.exact(Count, Population)))) %>%
-  unnest(ptest) %>% 
-  select(Count, Population, estimate, conf.low, conf.high)
-   
-
 f2 <- function(df, x, n) {
   df %>% 
     # rename data variables to match arguments
-  rename_with(~ "x", {{ x }}) %>% 
+    rename_with(~ "x", {{ x }}) %>% 
     rename_with(~ "n", {{ n }}) %>% 
     group_by(x, n) %>%
     nest() %>%
@@ -41,6 +17,33 @@ f2 <- function(df, x, n) {
     unnest(ptest) %>% 
     select(x, n, estimate, conf.low, conf.high)
   }
+
+df <- data.frame(id = c("A", "B", "C"),  x = c(0, 10, 10),
+                 n = c(100, 400, 400))
+df %>% 
+  group_by(id) %>% 
+  group_modify(~ f2(.x, x, n))
+
+# GROUPING BY RECORD ID WOULD OCCUR OUTSIDE WORKING FUNCTION
+# nest() and unnest() would probably not be needed
+# key is using tidy() to put test results in data frame
+# df %>%
+#   group_by(id, x, n) %>%
+#   nest() %>%
+#   mutate(ptest = map(data, ~ tidy(poisson.exact(x, n)))) %>%
+#   unnest(ptest) %>% 
+#   select(x, n, estimate, conf.low, conf.high)
+
+# with real data
+df2 <- read_tsv(here("data-raw", "Poisson rates.txt"), n_max = 6) %>% 
+  select(State = States, Count, Population, Crude_Rate = `Crude Rate`) 
+
+# df2 %>%
+#   group_by(State, Count, Population) %>%
+#   nest() %>%
+#   mutate(ptest = map(data, ~ tidy(poisson.exact(Count, Population)))) %>%
+#   unnest(ptest) %>% 
+#   select(Count, Population, estimate, conf.low, conf.high)
 
 df2 %>% 
   filter(State == "New Hampshire") %>% 
@@ -51,8 +54,6 @@ df2 %>%
   group_modify(~ f2(.x, Count, Population))
 
 # fix names to match
-df %>% 
-  group_by(id) %>% 
-  group_modify(~ f2(.x, Count, Population))
+
 
 
